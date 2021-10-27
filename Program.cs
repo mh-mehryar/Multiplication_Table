00436c13dd100963cb5bfa76e8d489bdd9e4798e
mh-mehryar <44663511+mh-mehryar@users.noreply.github.com>
2021-10-27 22:28:33 +0330

    Multiplication_table

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int [,] myarray = new int [10,10];
+          for (int i = 1; i <=10; i++)
+           {
+             for (int j = 1; j <=10; j++)
+             {
+               int Result = i * j;
+               Console.Write(Result.ToString() +"\t");
+                
+             } 
+             System.Console.WriteLine();
+           }
+        }
+    }
+}
